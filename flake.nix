bc206703cf6c4e8348a4b42ee768e3577e7b8e01
root <root@zits.home.arpa>
2023-05-01 13:58:40 -0700

    switch to fale re gloabl pkgs

--- flake.nix
+++ flake.nix
@@ -18,34 +18,13 @@
         ./configuration.nix
         home-manager.nixosModules.home-manager
         {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
+          home-manager.useGlobalPkgs = false;
+          home-manager.useUserPackages = false;
           home-manager.users.truman = import ./home.nix;
 
           # Optionally, use home-manager.extraSpecialArgs to pass
           # arguments to home.nix
         }
-      ];
-    };
-
-    nixosConfigurations.truman = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ({ pkgs, ... }: {
-          boot.isContainer = true;
-          system.stateVersion = "22.05";
-
-
-          # Network configuration.
-          networking.useDHCP = false;
-          # networking.firewall.allowedTCPPorts = [ 80 ];
-
-          # Enable a web server.
-          services.httpd = {
-            enable = true;
-            adminAddr = "morty@example.org";
-          };
-        })
       ];
     };
   };
