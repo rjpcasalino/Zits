597b81514be2fe7bac1a7797e9e16659488b9e4d
root <root@zits.home.arpa>
2023-05-12 07:26:47 -0700

    update flake and lock

--- flake.nix
+++ flake.nix
@@ -5,28 +5,54 @@
     nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
     home-manager = {
       #url = github:nix-community/home-manager/release-22.11;
-      url = github:nix-community/home-manager/master;
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   # Accepting unexpected attributes in argument set i.e., @
-  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
+    let
+      gnome = { pkgs, ... }: {
+        config = {
+          services.xserver.enable = true;
+          services.xserver.displayManager.gdm.enable = true;
+          services.xserver.desktopManager.gnome.enable = true;
+          environment.gnome.excludePackages =
+            (with pkgs; [ gnome-photos gnome-tour ]) ++ (with pkgs.gnome; [
+              cheese # webcam tool
+              gnome-music
+              gedit # text editor
+              epiphany # web browser
+              geary # email reader
+              gnome-characters
+              tali # poker game
+              iagno # go game
+              hitori # sudoku game
+              atomix # puzzle game
+              yelp # Help view
+              gnome-contacts
+              gnome-initial-setup
+            ]);
+          programs.dconf.enable = true;
+          environment.systemPackages = with pkgs; [ gnome.gnome-tweaks ];
+        };
+      };
+    in {
+      nixosConfigurations.zits = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.truman = import ./home.nix;
 
-    nixosConfigurations.zits = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.users.truman = import ./home.nix;
-
-          # Optionally, use home-manager.extraSpecialArgs to pass
-          # arguments to home.nix
-        }
-      ];
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
+      };
     };
-  };
 }
