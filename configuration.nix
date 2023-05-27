086d816fd8c3a0b31b40edff281be444029f92b4
root <root@zits.home.arpa>
2023-05-27 16:06:25 -0700

    remove doas;update flake

--- configuration.nix
+++ configuration.nix
@@ -12,8 +12,6 @@
   boot.loader.grub.useOSProber = true;
 
   boot.kernelPackages = pkgs.linuxPackages_6_2;
-  # make the kernel use amdgpu early
-  boot.initrd.kernelModules = [ "amdgpu" ];
   boot.kernelModules = [ "kvm-amd" "iwlwifi" ];
   # this is for wifi and bluetooth antenna
   boot.extraModulePackages = [ config.boot.kernelPackages.rtl88x2bu ];
@@ -119,7 +117,6 @@
     vscode
     curl
     cwm
-    doas
     direnv
     ed
     vim
@@ -238,7 +235,8 @@
     displayManager.startx.enable = true;
     windowManager.cwm.enable = true;
     windowManager.i3.enable = false;
-    videoDrivers = [ "amdgpu" ];
+    # this will pick amdgpu by default
+    videoDrivers = [ "modesetting" ];
   };
 
   virtualisation.docker = {
