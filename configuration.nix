db36a61a444377ab1609d15986899a189259110a
root <root@zits.home.arpa>
2023-07-12 21:21:29 -0700

    rearrange things and start using resolvd so we can use resolvctl wow...anyway some other cleanup

--- configuration.nix
+++ configuration.nix
@@ -4,12 +4,12 @@
   imports = [
     ./hardware-configuration.nix
   ];
-
-  # Use the systemd-boot EFI boot loader.
+  
+  ## boot ##
+  # systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.useOSProber = true;
-
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelModules = [ "kvm-amd" "iwlwifi" "k10temp" ];
   # this is for wifi and bluetooth antenna
@@ -19,12 +19,20 @@
     options iwldvm force_cam=Y
   '';
   boot.binfmt.emulatedSystems = [ "aarch64-linux" "armv6l-linux" ];
+  # #
 
   # SOPS
   sops.defaultSopsFile = "/home/rjpc/secrets/zits.yaml";
   sops.validateSopsFiles = false;
   sops.secrets."wireless.env" = { };
+  # #
 
+  # Security
+  security.rtkit.enable = true;
+  security.doas.enable = true;
+  # #
+
+  # Networking #
   networking.wireless.environmentFile = config.sops.secrets."wireless.env".path;
   networking.hostName = "zits";
   networking.wireless.enable =
@@ -46,7 +54,6 @@
   networking.nat.enable = false;
   # networking.nat.internalInterfaces = [ ];
   # networking.nat.externalInterface = "enp5s0";
-
   # see: https://discourse.nixos.org/t/a-fast-way-for-modifying-etc-hosts-using-networking-extrahosts/4190
   # note: the hosts mode is to allow vpn split to work for mct
   environment.etc.hosts.mode = "0644";
@@ -55,39 +62,33 @@
   networking.extraHosts = ''
     172.17.0.1 host.docker.internal
   '';
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
+  # The global useDHCP flag is deprecated
   networking.useDHCP = false;
   networking.interfaces.enp6s0.useDHCP = true;
   networking.interfaces.wlp5s0.useDHCP = true;
   networking.nameservers = [ "192.168.0.19" ];
   networking.enableIPv6 = true;
 
-  # Open ports in the firewall.
-  # only synergy thus far...
+  # Open ports in firewall
+  # only synergy thus far
   networking.firewall.allowedTCPPorts = [ 24800 51413 ];
   # networking.firewall.allowedUDPPorts = [ 53 ];
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
-  # services.resolved.enable = true;
+  services.resolved.enable = true;
+  # # 
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
+  # Internationalisation properties #
   i18n.defaultLocale = "en_US.UTF-8";
+  # #
 
-  # General Font Stuff #
+  # Font Stuff #
   console = {
     earlySetup = true;
     font = "${pkgs.terminus_font}/share/consolefonts/ter-v16n.psf.gz";
     packages = with pkgs; [ terminus_font ];
     keyMap = "us";
   };
-
   fonts.fonts = with pkgs; [
     nerdfonts
     noto-fonts
@@ -95,20 +96,157 @@
     noto-fonts-emoji
     liberation_ttf
   ];
+  # #
 
   # TIME ZONE
   time.timeZone = "America/Los_Angeles";
+  # #
 
-  # nixpkgs
-  # TODO: clean up?
+  # nix and nixpkgs #
   # FIXME: seems config.nix conflicts with this
   nixpkgs.config.allowUnfree = true;
-  # for BLE stuff? broken? see above
+  # for BLE stuff? broken?
   # nixpkgs.config.segger-jlink.acceptLicense = true;
+  nix = {
+    package = pkgs.nixUnstable;
+    settings.auto-optimise-store = true;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
+  # #
 
-  ## MPV OVERLAY ##
-  ## if you want vlc back just
-  ## use same overlay
+  # Android
+  programs.adb.enable = true;
+  # Steam
+  programs.steam.enable = true;
+
+  programs.bash.promptInit = ''
+    PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\]\n"
+  '';
+
+  # Linux console #
+  services.kmscon.enable = false;
+  # General Purpose Mouse daemon, which enables mouse support in virtual consoles
+  services.gpm.enable = true;
+  # #
+
+  # FIXME
+  # this wasn't working so do it
+  # hard way via xinitrc
+  services.redshift.enable = false;
+  # for redshift
+  location.latitude = 47.36;
+  location.longitude = -122.19;
+  location.provider = "manual";
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+  services.openssh.settings.X11Forwarding = false;
+  # #
+
+  # keybase service
+  services.keybase.enable = false;
+  
+  # CUPS and SANE #
+  services.printing.enable = true;
+  # If true and not using printer often
+  # the jounrnal will get polluted with messages...
+  # also needed for geoclue and redshift?
+  services.avahi.enable = true;
+  # Enable SANE for scanning
+  hardware.sane.enable = true;
+  services.avahi.nssmdns = true;
+  # Needed since this is an HP scanner 
+  # hardware.sane.extraBackends = [ pkgs.hplipWithPlugin ];
+  # use below as above does not seem to scan but sees scanner
+  # see: https://github.com/alexpevzner/sane-airscan
+  hardware.sane.extraBackends = [ pkgs.sane-airscan ];
+  # for a WiFi printer
+  services.avahi.openFirewall = true;
+  services.printing.drivers = [ pkgs.hplip ];
+  # #
+
+  # Audio and Sound #
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    audio.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    jack.enable = true;
+    wireplumber.enable = true;
+    pulse.enable = true;
+  };
+  # #
+  
+  # related to bluetooth HSP/HFP mode
+  # see: https://nixos.wiki/wiki/Bluetooth#No_audio_when_using_headset_in_HSP.2FHFP_mode
+  hardware.enableAllFirmware = true;
+  hardware.enableRedistributableFirmware = true;
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      General = {
+        Name = "Zits";
+        ControllerMode = "dual";
+        FastConnectable = "true";
+        Experimental = "true";
+      };
+      Policy = { AutoEnable = "true"; };
+      LE = { EnableAdvMonInterleaveScan = "true"; };
+    };
+  };
+
+  # X11 et al #
+  hardware.opengl.enable = true;
+  hardware.opengl.driSupport = true;
+  services.xserver = {
+    enable = true;
+    layout = "us";
+    autorun = false;
+    exportConfiguration = true;
+    displayManager.startx.enable = true;
+    displayManager.setupCommands = ''
+      ${pkgs.xorg.xrandr}/bin/xrandr --output "DP-1" --primary --rotate normal --output "HDMI-1" --rotate normal --left-of "DP-1"
+    '';
+    windowManager.cwm.enable = true;
+    windowManager.i3.enable = false;
+    # this will pick amdgpu by default
+    videoDrivers = [ "modesetting" ];
+    libinput = {
+      enable = true;
+      mouse = {
+        middleEmulation = true;
+        tapping = false;
+        tappingButtonMap = "lmr";
+      };
+    };
+  };
+  # #
+
+  xdg.portal.enable = false;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+
+  # Docker et al
+  virtualisation.docker = {
+    enable = true;
+    logDriver = "journald";
+    liveRestore = true;
+    package = pkgs.docker;
+  };
+  virtualisation.docker.autoPrune = {
+    enable = true;
+    flags = [ "--all, --volumes" ];
+  };
+  virtualisation.podman.enable = false;
+  virtualisation.libvirtd.enable = false;
+  virtualisation.virtualbox.host.enable = false;
+  virtualisation.lxd.enable = false;
+
+  ## overlays ##
   nixpkgs.overlays = [
     (self: super: {
       mpv-unwrapped = super.mpv-unwrapped.override {
@@ -119,15 +257,9 @@
       };
     })
   ];
+  # #
 
-  nix = {
-    package = pkgs.nixUnstable;
-    settings.auto-optimise-store = true;
-    extraOptions = ''
-      experimental-features = nix-command flakes
-    '';
-  };
-
+  # system and users # 
   environment.systemPackages = with pkgs; [
     arandr
     awscli2
@@ -150,7 +282,7 @@
     jq
     libbluray
     libaacs
-    mpv-unwrapped
+    mpv-unwrapped # see overlays
     microsoft-edge
     minikube
     nixpkgs-fmt
@@ -178,128 +310,6 @@
     xorg.xev
     zoom-us
   ];
-
-  # Android
-  programs.adb.enable = true;
-  # Steam
-  programs.steam.enable = true;
-  programs.bash.promptInit = ''
-    PS1="\n\[\033[01;32m\]\u $\[\033[00m\]\[\033[01;36m\] \w >\[\033[00m\]\n"
-  '';
-  # FIXME: remove?
-  services.kmscon.enable = false;
-  # General Purpose Mouse daemon, which enables mouse support in virtual consoles
-  services.gpm.enable = true;
-  # FIXME
-  # this wasn't working so do it
-  # hard way via xinitrc
-  services.redshift.enable = false;
-  # for redshift
-  location.latitude = 47.36;
-  location.longitude = -122.19;
-  location.provider = "manual";
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-  services.openssh.settings.X11Forwarding = false;
-  # keybase service
-  services.keybase.enable = false;
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  # If true and not using printer often
-  # the jounrnal will get polluted with messages...
-  # also needed for geoclue and redshift?
-  services.avahi.enable = true;
-  # Enable SANE for scanning
-  hardware.sane.enable = true;
-  services.avahi.nssmdns = true;
-  # Needed since this is an HP scanner 
-  # hardware.sane.extraBackends = [ pkgs.hplipWithPlugin ];
-  # use below as above does not seem to scan but sees scanner
-  # see: https://github.com/alexpevzner/sane-airscan
-  hardware.sane.extraBackends = [ pkgs.sane-airscan ];
-  # for a WiFi printer
-  services.avahi.openFirewall = true;
-  services.printing.drivers = [ pkgs.hplip ];
-
-  hardware.pulseaudio.enable = false;
-  # TODO: move this
-  # also, I removed sound.enable
-  security.rtkit.enable = true;
-  security.doas.enable = true;
-  services.pipewire = {
-    enable = true;
-    audio.enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    jack.enable = true;
-    wireplumber.enable = true;
-    pulse.enable = true;
-  };
-
-  # TODO
-  # look into LE settings
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-    settings = {
-      General = {
-        Name = "Zits";
-        ControllerMode = "dual";
-        FastConnectable = "true";
-        Experimental = "true";
-      };
-      Policy = { AutoEnable = "true"; };
-      LE = { EnableAdvMonInterleaveScan = "true"; };
-    };
-  };
-
-  # related to bluetooth HSP/HFP mode
-  # see: https://nixos.wiki/wiki/Bluetooth#No_audio_when_using_headset_in_HSP.2FHFP_mode
-  hardware.enableAllFirmware = true;
-  hardware.enableRedistributableFirmware = true;
-
-  hardware.opengl.enable = true;
-  hardware.opengl.driSupport = true;
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-    layout = "us";
-    autorun = false;
-    exportConfiguration = true;
-    displayManager.startx.enable = true;
-    displayManager.setupCommands = ''
-      ${pkgs.xorg.xrandr}/bin/xrandr --output "DP-1" --primary --rotate normal --output "HDMI-1" --rotate normal --left-of "DP-1"
-    '';
-    windowManager.cwm.enable = true;
-    windowManager.i3.enable = false;
-    # this will pick amdgpu by default
-    videoDrivers = [ "modesetting" ];
-    libinput = {
-      enable = true;
-      mouse = {
-        middleEmulation = true;
-        tapping = false;
-        tappingButtonMap = "lmr";
-      };
-    };
-  };
-  xdg.portal.enable = false;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-  virtualisation.docker = {
-    enable = true;
-    logDriver = "journald";
-    liveRestore = true;
-    package = pkgs.docker;
-  };
-  virtualisation.docker.autoPrune = {
-    enable = true;
-    flags = [ "--all, --volumes" ];
-  };
-  virtualisation.podman.enable = false;
-  virtualisation.libvirtd.enable = false;
-  virtualisation.virtualbox.host.enable = false;
-  virtualisation.lxd.enable = false;
-
   users.users.rjpc = {
     isNormalUser = true;
     extraGroups = [
@@ -316,13 +326,10 @@
     ];
     shell = "${pkgs.bashInteractive}${pkgs.bashInteractive.shellPath}";
   };
-
-  # this should be used for work stuff moving forward
   users.users.truman = {
     isNormalUser = true;
     extraGroups = [ "wheel" "audio" "docker" "sound" ];
     shell = "${pkgs.zsh}${pkgs.zsh.shellPath}";
   };
-
   system.stateVersion = "22.11";
 }
