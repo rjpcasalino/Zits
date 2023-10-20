1edccdb2a8916ae6e0ce13051bee35c3f7cf451c
rjpc <rjpc@rjpc.net>
2023-10-20 11:25:11 -0700

    update flake

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs = { url = "github:nixos/nixpkgs"; };
+    home-manager = { url = "github:nix-community/home-manager"; };
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -10,13 +11,19 @@
   };
 
   # Accepting unexpected attributes in argument set i.e., @
-  outputs = inputs@{ self, nixpkgs, sops-nix, ... }: {
-    nixosModules = {};
+  outputs = inputs@{ self, nixpkgs, home-manager, sops-nix, ... }: {
+    nixosModules = { };
     nixosConfigurations.zits = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = with self.nixosModules; [
         ./configuration.nix
         sops-nix.nixosModules.sops
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.rjpc = import ./home.nix;
+        }
       ];
     };
   };
