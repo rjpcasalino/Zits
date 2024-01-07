c35200e62d641d074abdd45056bb67cc9644f1e4
rjpc <rjpc@rjpc.net>
2024-01-06 22:01:02 -0800

    update flake again and update hm userpackages thingy

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = false;
+          home-manager.useUserPackages = true;
           home-manager.users.rjpc = import ./home.nix;
         }
       ];
